2a35f7160b71f00696a053d0719d586807c0a6c5
Rozheliuk Vitalii <rozhelluk.py@gmail.com>
2023-03-16 03:20:18 +0200

    modified:   init.lua modified:   plugins/community.lua modified:   plugins/user.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,11 +1,7 @@
 return {
-  -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
   { import = "astrocommunity.markdown-and-latex.glow-nvim" },
-  { import = "astrocommunity.utility.noice-nvim" },
+  -- { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.pack.python" },
   {
     "linux-cultist/venv-selector.nvim",
@@ -14,5 +10,4 @@ return {
   },
   { import = "astrocommunity.editing-support.neogen" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
-
 }
