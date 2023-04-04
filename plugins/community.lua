69c6450ab808cedd5549615884ea17ce3acc7e62
Rozheliuk Vitalii <rozhelluk.py@gmail.com>
2023-04-04 16:07:47 +0300

    fix(plugins): fixed cinnamon and auto-save plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,14 +1,11 @@
 return {
-  "AstroNvim/astrocommunity",
+  { "AstroNvim/astrocommunity" },
   { import = "astrocommunity.markdown-and-latex.glow-nvim" },
   -- { import = "astrocommunity.utility.noice-nvim" },
   -- { import = "astrocommunity.pack.python" },
-  {
-    "linux-cultist/venv-selector.nvim",
-    opts = { name = ".env" },
-    keys = { { "<leader>lv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" } },
-  },
   { import = "astrocommunity.editing-support.neogen" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
   { import = "astrocommunity.motion.vim-matchup" },
+  { import = "astrocommunity.scrolling.cinnamon-nvim" },
+  { import = "astrocommunity.editing-support.auto-save-nvim" },
 }
