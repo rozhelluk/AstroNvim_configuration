dba9ba4bc5f26469b9cb72763ba3935d011cce30
Rozheliuk Vitalii <rozhelluk.py@gmail.com>
2023-04-26 03:01:05 +0300

    feat(plugins): add image.nvim

--- plugins/user.lua
+++ plugins/user.lua
@@ -15,4 +15,49 @@ return {
     -- modify config table if necessary to configure `nvim-dap-ui`
     return config
   end,
+  -- { "m00qek/baleia.nvim", tag = "v1.3.0" },
+  -- {
+  --   "samodostal/image.nvim",
+  --   config = function(_, opts)
+  --     require("image").setup {
+  --       render = {
+  --         min_padding = 5,
+  --         show_label = true,
+  --         use_dither = true,
+  --         foreground_color = false,
+  --         background_color = false,
+  --       },
+  --       events = {
+  --         update_on_nvim_resize = true,
+  --       },
+  --     }
+  --   end,
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --   },
+  --   event = "VeryLazy",
+  -- },
+  {
+    "samodostal/image.nvim",
+    enabled = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "m00qek/baleia.nvim",
+    },
+    config = function()
+      require("image").setup {
+        render = {
+          min_padding = 5,
+          show_label = true,
+          use_dither = true,
+          foreground_color = true,
+          background_color = true,
+        },
+        events = {
+          update_on_nvim_resize = true,
+        },
+      }
+    end,
+    event = "VeryLazy",
+  },
 }
