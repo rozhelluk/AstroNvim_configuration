e7b72503978c4a0798fe982e9d7f90ae677ebc15
Rozheliuk Vitalii <rozhelluk.py@gmail.com>
2023-12-03 05:40:25 +0200

    feat(filetype): set htmldjango filetype for html

--- init.lua
+++ init.lua
@@ -2,23 +2,23 @@ return {
   --
   -- Configure AstroNvim updates
   updater = {
-  --   remote = "origin", -- remote to use
+    --   remote = "origin", -- remote to use
     channel = "nightly", -- "stable" or "nightly"
-  --   version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-  --   branch = "nightly", -- branch name (NIGHTLY ONLY)
-  --   commit = nil, -- commit hash (NIGHTLY ONLY)
-  --   pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
-  --   skip_prompts = false, -- skip prompts about breaking changes
-  --   show_changelog = true, -- show the changelog after performing an update
-  --   auto_quit = false, -- automatically quit the current session after a successful update
-  --   remotes = { -- easily add new remotes to track
-  --     --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
-  --     --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
-  --     --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
-  --   },
+    --   version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
+    -- branch = "nightly", -- branch name (NIGHTLY ONLY)
+    --   commit = nil, -- commit hash (NIGHTLY ONLY)
+    --   pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
+    --   skip_prompts = false, -- skip prompts about breaking changes
+    --   show_changelog = true, -- show the changelog after performing an update
+    --   auto_quit = false, -- automatically quit the current session after a successful update
+    --   remotes = { -- easily add new remotes to track
+    --     --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
+    --     --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
+    --     --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
+    --   },
   },
   -- Set colorscheme to use
-  colorscheme = "gruvbox-baby",
+  -- colorscheme = "nord",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,
@@ -29,7 +29,7 @@ return {
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = true,     -- enable or disable format on save globally
+        enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
         },
@@ -76,7 +76,7 @@ return {
     icons.GitRenamed = text_icons.GitRenamed
     icons.Selected = text_icons.Selected
 
-    vim.cmd [[ autocmd BufNewFile,BufRead *.html set filetype=html ]]
+    vim.cmd [[ autocmd BufNewFile,BufRead *.html set filetype=htmldjango ]]
     vim.cmd [[ autocmd FileType htmldjango,html,javascript,lua,yaml,yml,cpp setlocal tabstop=2 shiftwidth=2 softtabstop=2 expandtab]]
     -- Set up custom filetypes
     -- vim.filetype.add {
